0d85b12d78ada291359b616edcc99fdef3ddda6d
Виталий Рощупкин <vitalya.cookie@gmail.com>
2020-12-03 15:25:24 +0500

    advent of code task 3 part 1

---
+++ Contests/Common/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Contests.Common
+{
+    public static class StringExtensions
+    {
+        public static string ReplacePosition(this string str, char newChar, int position)
+        {
+            return str.Substring(0, position) + newChar + str.Substring(position + 1);
+        }
+    }
+}
---
+++ Contests/Common/Tests/StringExtensionsTest.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Contests.Common.Tests
+{
+    public class StringExtensionsTest
+    {
+        [TestCase("abc", 'x', 0, "xbc")]
+        [TestCase("abc", 'x', 1, "axc")]
+        [TestCase("abc", 'x', 2, "abx")]
+        public void TestReplacePosition(string initial, char newChar, int position, string expected)
+        {
+            initial.ReplacePosition(newChar, position).Should().BeEquivalentTo(expected);
+        }
+    }
+}
